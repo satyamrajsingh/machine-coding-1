8e96ed898fa2b7ca9abac851d17caaaba25d41c0
Satyam Raj Singh <satyamrajsingh.31@gmail.com>
2024-06-28 23:50:25 +0530

    Checking again

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Checking my commit");
         System.out.println("Hello world! I am Satyam");
+        System.out.println("Checkking again, Mike check");
     }
 }
